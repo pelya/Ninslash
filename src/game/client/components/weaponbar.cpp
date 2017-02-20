58d9dd8655282adef12d648841f9ed837016a153
Sergii Pylypenko <x.pelya.x@gmail.com>
2017-02-20 23:21:26 +0200

    Chat and scores buttons always enabled

--- src/game/client/components/weaponbar.cpp
+++ src/game/client/components/weaponbar.cpp
@@ -77,7 +77,7 @@ void CWeaponbar::OnFingerRelease()
 
 void CWeaponbar::OnRender()
 {
-	if(m_pClient->m_Snap.m_SpecInfo.m_Active)
+	if(m_pClient->m_Snap.m_pGameInfoObj)
 	{
 		m_Touching = false;
 		return;
